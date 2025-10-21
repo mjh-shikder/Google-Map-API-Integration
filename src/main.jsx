92b367faa16fd0883b4850b89711feffb0d9e366
mjh-shikder <mjhsikder@gmail.com>
2025-10-21 20:33:42 +0600

    could not get the api key yet

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { googleMapApiKey } from './map-api-key.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={ googleMapApiKey }>
+      <App></App>
+    </APIProvider>
   </StrictMode>,
 )
